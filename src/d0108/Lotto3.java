161d5e9622f17b9b708a3de9238bd91d9ed99445
Administrator <Administrator@211.201.71.183>
2025-01-09 11:26:05 +0900

    dd

--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -7,7 +7,7 @@ public class Lotto3 {
 	public static void main(String[] args) {
 		int[] nums = new int[3];
 		Random r = new Random();
-		for(int i = 0;i<3; i++) {
+		for(int i=0;i<3;i++) {
 			nums[i] = r.nextInt(100)+1;
 		}
 
